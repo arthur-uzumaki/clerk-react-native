72ffd2af482e1d6f5e4a4d432a8882b64f67ca66
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-06-21 15:19:02 -0300

    feat: criando um butão pra logout

--- expo-auth-clerk/src/app/(auth)/index.tsx
+++ expo-auth-clerk/src/app/(auth)/index.tsx
@@ -1,12 +1,22 @@
-import { Text, View } from "react-native";
+import { Button } from "@/components/Button";
+import { useAuth , useUser} from "@clerk/clerk-expo";
+import { Image, Text, View } from "react-native";
 
 export default function Home() {
-
+  const {signOut} = useAuth()
+  const {user } = useUser()
   return (
     <View className="flex-1 p-8 items-center justify-center gap-3">
+      <Image source={{uri: user?.imageUrl}} className="rounded-full h-24 w-24"/>
       <Text className="font-bold text-[18px]">
-        Olá, usuário
+      Hello 🖐️, {user?.firstName} 
       </Text>
+
+      <Button 
+      icon="log-out" 
+      title="Logout "
+      onPress={() => signOut()}
+      />
     </View>
   )
 }
