b42602652ad564d59c36275bd543578dbeb6186a
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-06-21 15:20:07 -0300

    closes #8

--- expo-auth-clerk/src/app/(auth)/index.tsx
+++ expo-auth-clerk/src/app/(auth)/index.tsx
@@ -1,21 +1,27 @@
 import { Button } from "@/components/Button";
-import { useAuth , useUser} from "@clerk/clerk-expo";
+import { useAuth, useUser } from "@clerk/clerk-expo";
 import { Image, Text, View } from "react-native";
 
 export default function Home() {
-  const {signOut} = useAuth()
-  const {user } = useUser()
+  const { signOut } = useAuth()
+  const { user } = useUser()
   return (
     <View className="flex-1 p-8 items-center justify-center gap-3">
-      <Image source={{uri: user?.imageUrl}} className="rounded-full h-24 w-24"/>
-      <Text className="font-bold text-[18px]">
-      Hello 🖐️, {user?.firstName} 
+      <Image
+        source={{ uri: user?.imageUrl }}
+        className="rounded-full h-24 w-24"
+      />
+
+      <Text
+        className="font-bold text-[18px]"
+      >
+        Hello 🖐️, {user?.firstName}
       </Text>
 
-      <Button 
-      icon="log-out" 
-      title="Logout "
-      onPress={() => signOut()}
+      <Button
+        icon="log-out"
+        title="Logout "
+        onPress={() => signOut()}
       />
     </View>
   )
